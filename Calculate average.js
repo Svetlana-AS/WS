11295d457535ae414970161e3deaf9c9718b4c12
Svetlana-AS <astaninasv1@gmail.com>
2023-09-11 16:33:31 +0200

    Is it even?

--- Calculate average.js
+++ Calculate average.js
@@ -1,11 +1,11 @@
 // Write a function which calculates the average of the numbers in a given list.
 
-function findAverage (array){
-    let sum=0
-    for(let  i= 0; i<array.length; i++){
-        sum += array[i]
-    }
-    const average = sum/array.length
-    return average
+function countSheeps(arrayOfSheep) {
+    let sum = 0
+    arrayOfSheep.map(el=> {
+        if(el=== true){
+            sum++;
+        }
+    })
+    return sum;
 }
-
---
+++ Is it even.js
@@ -0,0 +1,6 @@
+// Is it even?
+
+function testEven(n) {
+    return   n % 2 === 0;
+}
+
