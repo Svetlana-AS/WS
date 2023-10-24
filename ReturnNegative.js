a8538be2913d5899899929b3baa5bd6ef9992501
Svetlana-AS <astaninasv1@gmail.com>
2023-10-24 13:54:27 +0200

    ReturnNegative

---
+++ ReturnNegative.js
@@ -0,0 +1,13 @@
+// In this simple assignment you are given a number and have to make it negative.
+//     But maybe the number is already negative?
+//
+//     Examples
+//     makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+
+function makeNegative(num) {
+    const makeNum = num > 0  ? num * -1    : num
+    return makeNum
+}
