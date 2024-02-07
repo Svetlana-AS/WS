1bbcec156bfbc27f95d953b6b979fec99555da11
Svetlana-AS <astaninasv1@gmail.com>
2024-02-07 12:11:16 +0100

    FindMultiplesOfaNumber

---
+++ FindMultiplesOfaNumber.js
@@ -0,0 +1,14 @@
+// Find Multiples of a Number
+// Example : (2, 6), the function should return [2, 4, 6] as 2, 4, and 6
+
+
+function findMultiples(integer, limit) {
+    let arrayResult = []
+
+    for (let i = integer; i<=limit; i+=integer)
+        arrayResult.push(i)
+
+    return arrayResult
+
+}
+
