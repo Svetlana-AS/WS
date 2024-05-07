d5abcf36b4755a3ac973968eca5461c208915171
Svetlana-AS <astaninasv1@gmail.com>
2024-05-07 22:20:48 +0200

    HeadsAndLegs

---
+++ HeadsAndLegs.js
@@ -0,0 +1,16 @@
+// [Heads, Legs] = [72, 200]
+//
+// VALID - [72, 200] =>             [44 , 28]
+//     [Chickens, Cows]
+//
+// INVALID - [72, 201] => "No solutions"
+
+
+function animals(heads, legs){
+   const cows = legs/2 - heads
+   const chickens = heads-cows
+
+
+}
+
+
