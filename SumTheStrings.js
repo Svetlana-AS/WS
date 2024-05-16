31178cf977004e62f094cb85e8db633daec83555
Svetlana-AS <astaninasv1@gmail.com>
2024-05-16 15:21:31 +0200

    SumTheString

---
+++ SumTheStrings.js
@@ -0,0 +1,13 @@
+// Sum The Strings
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+
+function sumStr(a,b) {
+
+    return String(Number(a) +Number(b))
+}
+
+
+function sumStr(a,b) {
+    return `${+a + +b}`;
+}
--- TemplateStrings.js
+++ TemplateStrings.js
@@ -1,4 +1,4 @@
 // Template Strings - шаблонные строки
 
 const TempleStrings = function(obj, featrue) {
-    return `${obj} are ${featrue}`
+    return `${obj} are ${featrue}`}
