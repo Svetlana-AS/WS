be0789842808d65cec001646187276d9094b4bc8
Svetlana-AS <astaninasv1@gmail.com>
2024-06-28 13:50:31 +0200

    KeepUpTheHoop

--- Parse nice int from char problem.js
+++ Parse nice int from char problem.js
@@ -17,3 +17,11 @@ class Solution {
     }
 
 }
+
+function hoopCount (n) {
+    if(n>=10) {
+        return "Great, now move on to tricks"
+    }else {
+        return "Keep at it until you get it"
+    }
+}
